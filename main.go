da244a5e2b3113131eef16c04f808aa69b0eaed3
vamshidhar-kasulabada <kasulavamshidhar8@gmail.com>
2025-03-12 12:08:36 +0530

    working project

--- main.go
+++ main.go
@@ -2,12 +2,24 @@ package main
 
 import "fmt"
 
-
-func log(value string) {
-    fmt.Println(value)
-}
-
 func main() {
-    log("project initialized")
+	var revenue float64
+	fmt.Print("Enter revenue: ")
+	fmt.Scan(&revenue)
+	var expense float64
+	fmt.Print("Enter expense: ")
+	fmt.Scan(&expense)
+	var taxRate float64
+	fmt.Print("Enter tax rate: ")
+	fmt.Scan(&taxRate)
+	calculateAndDisplayProfit(revenue, expense, taxRate)
 }
 
+func calculateAndDisplayProfit(revenue float64, expense float64, taxRate float64) {
+	var ebt float64 = revenue - expense
+	fmt.Println("expense before tax", ebt)
+	var profit float64 = ebt * (1 - taxRate/100)
+	fmt.Println("profit:", profit)
+	var ratio = ebt / profit
+	fmt.Println("ratio of ebt to profit:", ratio)
+}
